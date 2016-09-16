272ecf80cfd4994dd07e572b2ac7c667dc4cf665
lindl <lin_donglian@163.com>
2016-09-16 17:00:35 +0800

    侧边栏

--- www/js/app.js
+++ www/js/app.js
@@ -1,6 +1,4 @@
-/**
- * Created by lin on 2016/9/15.
- */
+
 angular.module('todo', ['ionic'])
 
     .controller('TodoCtrl', function($scope) {
@@ -10,4 +8,31 @@ angular.module('todo', ['ionic'])
             { title: 'cccc' },
             { title: 'dddd' }
         ];
+        // 创建并载入模型
+        $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
+            $scope.taskModal = modal;
+        }, {
+            scope: $scope,
+            animation: 'slide-in-up'
+        });
+
+// 表单提交时调用
+        $scope.createTask = function(task) {
+            $scope.tasks.push({
+                title: task.title
+            });
+            $scope.taskModal.hide();
+            task.title = "";
+        };
+
+// 打开新增的模型
+        $scope.newTask = function() {
+            $scope.taskModal.show();
+        };
+
+// 关闭新增的模型
+        $scope.closeNewTask = function() {
+            $scope.taskModal.hide();
+        };
     });
+
