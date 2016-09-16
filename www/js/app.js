4b966915d978f8fa3f2cda159af3fad434eb1918
lindl <lin_donglian@163.com>
2016-09-16 17:31:50 +0800

    侧边栏

--- www/js/app.js
+++ www/js/app.js
@@ -1,23 +1,22 @@
-
 angular.module('todo', ['ionic'])
 
-    .controller('TodoCtrl', function($scope) {
+        .controller('TodoCtrl', function ($scope, $ionicModal) {
         $scope.tasks = [
-            { title: 'aaaa' },
-            { title: 'bbbb' },
-            { title: 'cccc' },
-            { title: 'dddd' }
+            {title: 'aaa'},
+            {title: 'bbb'},
+            {title: 'ccc'},
+            {title: 'ddd'}
         ];
         // 创建并载入模型
-        $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
+        $ionicModal.fromTemplateUrl('new-task', function (modal) {
             $scope.taskModal = modal;
         }, {
             scope: $scope,
             animation: 'slide-in-up'
         });
 
-// 表单提交时调用
-        $scope.createTask = function(task) {
+        // 表单提交时调用
+        $scope.createTask = function (task) {
             $scope.tasks.push({
                 title: task.title
             });
@@ -25,14 +24,14 @@ angular.module('todo', ['ionic'])
             task.title = "";
         };
 
-// 打开新增的模型
-        $scope.newTask = function() {
+        // 打开新增的模型
+        $scope.newTask = function () {
             $scope.taskModal.show();
         };
 
-// 关闭新增的模型
-        $scope.closeNewTask = function() {
+        // 关闭新增的模型
+        $scope.closeNewTask = function () {
             $scope.taskModal.hide();
         };
-    });
 
+    });
